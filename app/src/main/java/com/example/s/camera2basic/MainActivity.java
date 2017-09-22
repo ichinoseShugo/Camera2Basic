7acb59404e654449e428bcbc4662d49fd5dbc2d2
ichinoseShugo <ichinose@srmtlab.org>
2017-09-22 23:03:30 +0900

    copy from sample

--- app/src/main/java/com/example/s/camera2basic/MainActivity.java
+++ app/src/main/java/com/example/s/camera2basic/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.s.camera2basic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
     }
+
 }
